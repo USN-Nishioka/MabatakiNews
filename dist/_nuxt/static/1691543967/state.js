fb4722bbce5e33b88a6a5f28c88ca9ae637130a3
USN-Nishioka <nishioka@usn-corp.com>
2023-08-09 10:19:44 +0900

    add dist

---
+++ dist/_nuxt/static/1691543967/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FMabatakiNews\u002F_nuxt\u002Fstatic\u002F1691543967",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FMabatakiNews\u002F",assetsPath:"\u002FMabatakiNews\u002F_nuxt\u002F",cdnURL:a}}}}(null));
