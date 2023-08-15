4127dda4547f946d8cf7f59a4e8c8e02b5218e4a
USN-Nishioka <nishioka@usn-corp.com>
2023-08-15 23:55:11 +0900

    仮デザインが完成

--- pages/show/_id.vue
+++ pages/show/_id.vue
@@ -1,23 +1,31 @@
 <template>
-  <div class="flex flex-col justify-center items-center min-h-screen">
-    <div>
-      <!--ニュース情報の表示-->
-      {{ news.title }}<br>
-      {{ news.source }}<br>
-      {{ news.author }}<br>
-      {{ news.published_at }}<br>
-      {{ news.description }}<br>
-      <a :href="news.url">{{ news.url }}</a><br>
-      <img :src="news.image" alt="news.image"><br>
-    </div>
-    <div>
+  <div class="bg-gray-100 min-h-screen">
+    <div class="max-w-5xl mx-auto p-8">
+      <!-- ニュース情報の表示 -->
+      <div class="rounded-lg shadow-lg bg-white p-6 mb-8">
+        <h1 class="text-3xl font-bold mb-4">
+          {{ news.title }}
+        </h1>
+        <img :src="news.image" alt="news.image" class="w-full rounded-lg mb-4">
+        <div class="text-sm mb-2">
+          <span class="font-semibold">Source:</span> {{ news.source }} |
+          <span class="font-semibold">Author:</span> {{ news.author }} |
+          <span class="font-semibold">Date:</span> {{ news.published_at }}
+        </div>
+        <p class="text-gray-700 mb-4">
+          {{ news.description }}
+        </p>
+        <a :href="news.url" class="text-blue-500 hover:text-blue-700 hover:underline">{{ news.url }}</a>
+      </div>
       <!-- フラッシュの表示 -->
-      <span class="text-center text-blue-800 text-2xl mb-4">{{ currentFlash }}</span>
-    </div>
-    <div>
+      <div class="rounded-lg shadow-lg bg-white p-6 mb-8">
+        <span class="block text-center text-blue-800 text-2xl mb-4">{{ currentFlash }}</span>
+      </div>
       <!-- フラッシュ表示速度調整 -->
-      <input v-model="speed" class="w-1/2" type="range" min="0" max="1000" @input="updateSpeed">
-      <span class="text-center mb-4">{{ speed }}</span>
+      <div class="rounded-lg shadow-lg bg-white p-6 flex items-center justify-between">
+        <span class="text-gray-700">Flash Speed: {{ speed }}</span>
+        <input v-model="speed" class="w-1/2 ml-4" type="range" min="0" max="1000" @input="updateSpeed">
+      </div>
     </div>
   </div>
 </template>
