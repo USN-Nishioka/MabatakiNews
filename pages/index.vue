7aab6d568eef4c223180d084407fdb850fedffe5
USN-Nishioka <nishioka@usn-corp.com>
2023-08-18 16:04:05 +0900

    indexを調整

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,12 @@
     <h1 class="text-4xl font-bold text-green-500 tracking-wider my-8">
       Mabataki NEWS
     </h1>
-    <NewsCard v-for="(news, index) in newsList" :key="`news${index}`" :news="news" />
+    <div class="flex flex-wrap justify-center w-full px-4 md:px-0">
+      <NewsCard v-for="(news, index) in newsList" :key="`news${index}`" :news="news" />
+    </div>
   </div>
 </template>
+
 
 <script>
 export default {
