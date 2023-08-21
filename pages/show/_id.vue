d07d7b1882f84e36aef82548e2d4fe65b1625489
USN-Nishioka <nishioka@usn-corp.com>
2023-08-21 18:39:55 +0900

    ニュース詳細ページが完成

--- pages/show/_id.vue
+++ pages/show/_id.vue
@@ -8,17 +8,36 @@
         {{ truncatedTitle }}
       </h1>
     </div>
+    <!-- カウントダウンの表示 -->
+    <div v-if="countdown > 0" class="z-10 absolute inset-x-0 flex flex-col justify-center h-full text-center text-white text-5xl">
+      {{ countdown }}
+    </div>
     <!-- フラッシュの表示 -->
-    <div id="flash" class="p-8 z-10 absolute inset-x-0 flex flex-col justify-center h-full">
+    <div v-else id="flash" class="p-8 z-10 absolute inset-x-0 flex flex-col justify-center h-full">
       <div class="p-6 mb-8">
-        <span class="block text-center text-white text-5xl mb-4">{{ currentFlash }}</span>
+        <span class="block text-center text-white text-5xl mb-3">{{ currentFlash }}</span>
       </div>
     </div>
-    <!-- その他情報 -->
+    <!-- フッター -->
     <div id="info" class="p-8 font-semibold text-white text-right absolute inset-x-0 bottom-0 z-10">
-      Link: <a :href="news.url" class="text-blue-500 hover:text-blue-700 underline mb-4">{{ news.source }}</a><br>
-      Author: {{ news.author }}<br>
-      Date: {{ news.published_at }}<br>
+      <!-- その他情報 -->
+      <div class="mb-3">
+        Link: <a :href="news.url" class="text-blue-500 hover:text-blue-700 underline mb-4">{{ news.source }}</a><br>
+        Author: {{ news.author }}<br>
+        Date: {{ news.published_at }}<br>
+      </div>
+      <!-- 再生後表示ボタン -->
+      <div class="z-10 inset-x-0 flex flex-col justify-center h-full text-center space-y-4">
+        <button class="bg-blue-500 text-white px-4 py-2 hover:bg-blue-600" @click="goToNextNews">
+          &gt;&gt; NEXT
+        </button>
+        <button class="bg-blue-500 text-white px-4 py-2 hover:bg-blue-600" @click="goToPrevNews">
+          &lt;&lt; PREV
+        </button>
+        <button class="bg-green-500 text-white px-4 py-2 hover:bg-green-600" @click="replayNews">
+          ✓ REPLAY
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -30,7 +49,8 @@ export default {
   data() {
     return {
       currentIndex: 0, // 現在表示しているインデックス
-      speed: 300, // レンジスライダーの値
+      speed: 350, // レンジスライダーの値
+      countdown: 3, // カウントダウンの初期値
 
       // ニュース情報
       news: {
@@ -63,7 +83,15 @@ export default {
   },
 
   mounted() {
-    this.updateSpeed(); // ここで初期インターバルをセット
+    // カウントダウンの実行
+    const countdownInterval = setInterval(() => {
+      if (this.countdown > 0) {
+        this.countdown--;
+      } else {
+        clearInterval(countdownInterval); // カウントダウンが終了したらインターバルをクリア
+        this.updateSpeed(); // フラッシュの初期インターバルをセット
+      }
+    }, this.speed);
 
     // ニュースを取得
     this.$axios.get(`http://127.0.0.1:8000/get_news/${this.$route.params.id}`).then((res) => {
@@ -75,21 +103,40 @@ export default {
   },
 
   beforeDestroy() {
-    // コンポーネントが破棄される前にインターバルをクリア
-    clearInterval(this.interval);
+    clearInterval(this.interval); // コンポーネントが破棄される前にインターバルをクリア
   },
 
   methods: {
     updateSpeed() {
-      // 既存のインターバルをクリア
-      clearInterval(this.interval);
-      // 新しいインターバルをセット
-      this.interval = setInterval(this.updateFlash, this.speed);
+      clearInterval(this.interval); // 既存のインターバルをクリア
+      this.interval = setInterval(this.updateFlash, this.speed); // 新しいインターバルをセット
     },
 
     updateFlash() {
-      // インデックスを更新し、配列の最後に達したら0にリセット
-      this.currentIndex = (this.currentIndex + 1) % this.news.flash.length;
+      // インデックスが配列の最後に達した場合
+      if (this.currentIndex >= this.news.flash.length - 1) {
+        clearInterval(this.interval); // インターバルをクリアしてflashの表示を停止
+        // 5s後に次のニュースに遷移
+        setTimeout(() => {
+          this.goToNextNews();
+        }, 5000);
+      } else {
+        this.currentIndex++; // インデックスを更新
+      }
+    },
+
+    goToPrevNews() {
+      const prevId = parseInt(this.$route.params.id, 10) - 1;
+      this.$router.push(`/show/${prevId}`);
+    },
+
+    goToNextNews() {
+      const nextId = parseInt(this.$route.params.id, 10) + 1;
+      this.$router.push(`/show/${nextId}`);
+    },
+
+    replayNews() {
+      this.$router.go(0);
     }
   }
 };
